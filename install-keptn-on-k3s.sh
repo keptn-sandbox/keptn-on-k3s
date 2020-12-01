cc0041c79928cd55edaec0a435b826ce0bc63692
Andreas Grabner <grabnerandi@gmx.at>
2020-12-01 11:43:45 +0100

    upgraded to latest services and updated sample action scripts
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- install-keptn-on-k3s.sh
+++ install-keptn-on-k3s.sh
@@ -271,8 +271,8 @@ function install_keptn {
       --from-literal="KEPTN_API_TOKEN=$(get_keptn_token)" \
       --from-literal="KEPTN_BRIDGE_URL=${PREFIX}://$FQDN/bridge"
 
-    apply_manifest_ns_keptn "https://raw.githubusercontent.com/keptn-contrib/dynatrace-service/0.10.0/deploy/service.yaml"
-    apply_manifest_ns_keptn "https://raw.githubusercontent.com/keptn-contrib/dynatrace-sli-service/0.7.0/deploy/service.yaml"
+    apply_manifest_ns_keptn "https://raw.githubusercontent.com/keptn-contrib/dynatrace-service/0.10.1/deploy/service.yaml"
+    apply_manifest_ns_keptn "https://raw.githubusercontent.com/keptn-contrib/dynatrace-sli-service/0.7.1/deploy/service.yaml"
 
     # lets make Dynatrace the default SLI provider (feature enabled with lighthouse 0.7.2)
     "${K3SKUBECTL[@]}" create configmap lighthouse-config -n keptn --from-literal=sli-provider=dynatrace || true 
