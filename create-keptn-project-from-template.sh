f5dc418e19f2ec812a01469e0ba053ad6e4bf4b4
Andreas Grabner <grabnerandi@gmx.at>
2021-03-02 16:50:30 +0100

    fixes
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- create-keptn-project-from-template.sh
+++ create-keptn-project-from-template.sh
@@ -41,11 +41,22 @@ if ! [ -d "${TEMPLATE_DIRECTORY}/${TEMPLATE_NAME}" ]; then
     exit 1
 fi
 
+# Validate that the specific project template has a shipyard
+if ! [ -f "${TEMPLATE_DIRECTORY}/${TEMPLATE_NAME}/shipyard.yaml" ]; then
+    echo "Keptn Project Template doesnt have a shipyard.yaml: ${TEMPLATE_DIRECTORY}/${TEMPLATE_NAME}/shipyard.yaml"
+    exit 1
+fi
+
+# switch to template directory
+currDir=$(pwd)
+echo "Switching to Template Directory"
+cd "${TEMPLATE_DIRECTORY}/${TEMPLATE_NAME}"
+
 #
 # Now lets create that Keptn project
 #
 echo "Create Keptn Project: ${PROJECT_NAME} from ${TEMPLATE_NAME}"
-keptn create project "${PROJECT_NAME}" --shipyard=./${TEMPLATE_DIRECTORY}/${TEMPLATE_NAME}/shipyard.yaml
+keptn create project "${PROJECT_NAME}" --shipyard=./shipyard.yaml
 
 #
 # Now we iterate through the template folder and add all resources on project level
@@ -56,17 +67,15 @@ do
     # if this is a directory we dont do anything!
     if [ -d "$tempFile" ]; then continue; fi;
 
-    # remove the trailing ./${TEMPLATE_DIRECTORY}/${TEMPLATE_NAME} of the tree output
-    localFileName=$(echo "${tempFile/.\${TEMPLATE_DIRECTORY}\/${TEMPLATE_NAME}\//}")
-
     # we are not re-uploading the shipyard.yaml nor do we iterate through the service_template subdirectories
     if [[ "${localFileName}" == *"shipyard.yaml"* ]]; then continue; fi
     if [[ "${localFileName}" == *"service_template"* ]]; then continue; fi
 
-    echo "Processing localFileName: ${tempFile}"
+    echo "Processing localFileName: ${localFileName}"
 
     # TODO - replace placeholders within FILES, e.g: PROJECT_NAME, STAGE_NAME, SERVICE_NAME, KEPTNS_BRIDGE_URL ...
-    remoteFileName=$(echo "${localFileName/KEPTN_PROJECT_NAME/$PROJECT_NAME}")
+    remoteFileName=$(echo "${localFileName/.\//}")
+    remoteFileName=$(echo "${remoteFileName/KEPTN_PROJECT_NAME/$PROJECT_NAME}")
     remoteFileName=$(echo "${remoteFileName/KEPTN_STAGE_NAME/$STAGE_NAME}")
     remoteFileName=$(echo "${remoteFileName/KEPTN_SERVICE_NAME/$SERVICE_NAME}")
 
@@ -75,10 +84,10 @@ do
     # is this file for a specific stage?
     if [[ "${localFileName}" == *"stage_"** ]]; then
         # TODO parse the name of the stage from the stage_STAGENAME/filename
-        keptn add-resource --project="${PROJECT_NAME}" --stage="${STAGE_NAME}" --resource="./${localFileName}" --resourceUri="{remoteFileName}"
+        keptn add-resource --project="${PROJECT_NAME}" --stage="${STAGE_NAME}" --resource="${localFileName}" --resourceUri="${remoteFileName}"
 
     else 
-        keptn add-resource --project="${PROJECT_NAME}" --resource="./${localFileName}" --resourceUri="{remoteFileName}"
+        keptn add-resource --project="${PROJECT_NAME}" --resource="${localFileName}" --resourceUri="${remoteFileName}"
     fi; 
 done 
 
@@ -101,13 +110,14 @@ if ! [[ "$SERVICE_NAME" == "none" ]]; then
             echo "Processing localFileName: ${tempFile}"
 
             # TODO - replace placeholders within FILES, e.g: PROJECT_NAME, STAGE_NAME, SERVICE_NAME, KEPTNS_BRIDGE_URL ...
-            remoteFileName=$(echo "${localFileName/KEPTN_PROJECT_NAME/$PROJECT_NAME}")
+            remoteFileName=$(echo "${localFileName/.\//}")
+            remoteFileName=$(echo "${remoteFileName/KEPTN_PROJECT_NAME/$PROJECT_NAME}")
             remoteFileName=$(echo "${remoteFileName/KEPTN_STAGE_NAME/$STAGE_NAME}")
             remoteFileName=$(echo "${remoteFileName/KEPTN_SERVICE_NAME/$SERVICE_NAME}")
 
             echo "Using remoteFileName: ${remoteFileName}"
 
-            keptn add-resource --project="${PROJECT_NAME}" --service="${SERVICE_NAME}" --resource="./${localFileName}" --resourceUri="{remoteFileName}"
+            keptn add-resource --project="${PROJECT_NAME}" --service="${SERVICE_NAME}" --resource="${localFileName}" --resourceUri="${remoteFileName}"
         fi
 
     done 
@@ -115,4 +125,5 @@ if ! [[ "$SERVICE_NAME" == "none" ]]; then
 fi
 
 
-
+# switch back to prev working dir
+cd "${currDir}"
