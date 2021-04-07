09ed7fa61d036013e05beceecbda99f05fffba6e
Andreas Grabner <grabnerandi@gmx.at>
2021-04-07 13:58:35 +0200

    updates for auto-remediation
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- keptn_project_templates/auto-remediation/generic-executor/action.triggered.poweroutageaction.py
+++ keptn_project_templates/auto-remediation/generic-executor/action.triggered.poweroutageaction.py
@@ -6,13 +6,13 @@ methodArg = ""
 if len(sys.argv) > 1:
     methodArg = sys.argv[1]
 
-print("This is the output of my poweroutage action handler python called for " + os.getenv('PROBLEMTITLE',""))
-print("Received the following message from the remediation action definition: "+ os.getenv("VALUE_MESSAGE", "NO MESSAGE"))
+print("This is the output of my poweroutage action handler python called for " + os.getenv('DATA_PROBLEM_PROBLEMTITLE',""))
+print("Received the following message from the remediation action definition: "+ os.getenv("DATA_ACTION_VALUE_MESSAGE", "NO MESSAGE"))
 print("This output will be part of the Dynatrace Problem Comment.")
 print("You can even pass links via markdown: [Click me](https://www.keptn.sh)")
 
 # Following are some more examples on environment variables
-# print("I also have some env variables, e.g: PID=" + os.getenv('PID', "") + ", CONTEXT=" + os.getenv('CONTEXT', ""))
-# print("SOURCE=" + os.getenv('SOURCE',""))
-# print("PROJECT=" + os.getenv('PROJECT',""))
-# print("PROBLEMTITLE=" + os.getenv('PROBLEMTITLE',""))
+# print("I also have some env variables, e.g: PID=" + os.getenv('DATA_PROBLEM_PID', "") + ", SHKEPTNCONTEXT=" + os.getenv('SHKEPTNCONTEXT', ""))
+# print("SOURCE=" + os.getenv('DATA_SOURCE',""))
+# print("PROJECT=" + os.getenv('DATA_PROJECT',""))
+# print("PROBLEMTITLE=" + os.getenv('DATA_PROBLEM_PROBLEMTITLE',""))
