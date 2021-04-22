23e9c8198fbe1110165e8ac11c03185c580d7204
henrikrexed <henrik.rexed@dynatrace.com>
2021-04-22 10:00:32 +0200

    First commit to add neoload support in the deployment of k3s

--- install-keptn-on-k3s.sh
+++ install-keptn-on-k3s.sh
@@ -706,24 +706,24 @@ gitea_createGitRepo(){
 
 install_neoload_service() {
 
-   if [ -z "$NL_WEB_HOST" ]; then
+   if [[  "$NL_WEB_HOST" == "none" ]]; then
      NL_WEB_HOST="neoload.saas.neotys.com"
    fi
 
-   if [ -z "$NL_WEBAPI_HOST" ]; then
+   if [[ "$NL_WEBAPI_HOST" == "none" ]]; then
      NL_WEBAPI_HOST="neoload-api.saas.neotys.com"
    fi
 
-   if [ -z "$NL_WEBUPLOAD_HOST" ]; then
+   if [[  "$NL_WEBUPLOAD_HOST" === "none" ]]; then
      NL_WEBUPLOAD_HOST="neoload-files.saas.neotys.com"
    fi
 
-   if [ -z "$NL_API_TOKEN" ]; then
+   if [[ "$NL_API_TOKEN" == "none"  ]]; then
       echo "The NeoLoad API TOKEN has not be configure. create the NL_API_TOKEN environment variable first"
       exit 1
    fi
 
-   if [ -z "$NL_ZONE_ID" ]; then
+   if [[ "$NL_ZONE_ID" == "none" ]]; then
       echo "The NeoLoad Zone ID has not be configured. create the NL_ZONE_ID environment variable first"
       exit 1
    fi
