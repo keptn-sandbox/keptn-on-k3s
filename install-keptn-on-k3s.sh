ba2e4471f34cc618e15687ce45bd4df1a330bd3d
henrikrexed <henrik.rexed@dynatrace.com>
2021-04-23 17:22:33 +0200

    First commit to add neoload support in the deployment of k3s

--- install-keptn-on-k3s.sh
+++ install-keptn-on-k3s.sh
@@ -770,8 +770,8 @@ install_neoload_service() {
   echo "Deleting jmeter-service"
   "${K3SKUBECTL[@]}" delete deployment jmeter-service -n keptn
    PODS=$("${K3SKUBECTL[@]}" -n "$NAMESPACE" get pods --no-headers | awk '{print $1}' | grep jmeter-service | tr '\n' ' ')
-   echo "Deleting pods $PODS"
-  "${K3SKUBECTL[@]}" -n "$NAMESPACE" delete pods "$PODS" --ignore-not-found
+   echo "Deleting pods $PODS "
+  "${K3SKUBECTL[@]}" -n "$NAMESPACE" delete pods "$PODS" --force --ignore-not-found
 
 
 }
