8288a1e3f8a199fc51aef1e1318083f7d0b9668d
Andreas Grabner <grabnerandi@gmx.at>
2021-07-14 15:52:48 +0200

    yet another fix
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- create-keptn-project-from-template.sh
+++ create-keptn-project-from-template.sh
@@ -165,7 +165,7 @@ do
             # now either create a single or multiple instances
             for (( instanceIx=1; instanceIx<=instanceCount; instanceIx++ ))
             do
-                INSTANCE_SERVICE_NAME=$(echo "{SERVICE_NAME/XXX/$instanceIx})"
+                INSTANCE_SERVICE_NAME=$(echo "{SERVICE_NAME/XXX/$instanceIx}")
 
                 if [ -d "./service_${SERVICE_NAME}/charts" ]; then 
                     echo "Onboard Keptn Service: ${INSTANCE_SERVICE_NAME} for project ${PROJECT_NAME} with provided helm charts"
