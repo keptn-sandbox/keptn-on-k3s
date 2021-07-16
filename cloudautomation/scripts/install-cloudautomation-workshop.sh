306d9c3c9bae71219dd52921e10904f9d110c501
Andreas Grabner <grabnerandi@gmx.at>
2021-07-16 16:12:56 +0200

    updates
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- cloudautomation/scripts/install-cloudautomation-workshop.sh
+++ cloudautomation/scripts/install-cloudautomation-workshop.sh
@@ -6,6 +6,7 @@ PREFIX="https"
 
 KEPTN_CONTROL_PLANE_DOMAIN=${KEPTN_CONTROL_PLANE_DOMAIN:-none}
 KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN=${KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN:-none}
+WORKSHOP_TENANT_INSTANCES=${WORKSHOP_TENANT_INSTANCES:-none}
 
 if [[ "$KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN" == "none" ]] || [[ "$KEPTN_CONTROL_PLANE_DOMAIN" == "none" ]]; then
   echo "Script needs control plain domain set in KEPTN_CONTROL_PLANE_DOMAIN, e.g: abc12345.cloudautomation.live.dynatrace.com"
@@ -13,10 +14,15 @@ if [[ "$KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN" == "none" ]] || [[ "$KEPTN_CONTROL
   exit 1
 fi
 
+if [[ "WORKSHOP_TENANT_INSTANCES" == "none" ]]; then 
+  echo "Specify an array with instance names in WORKSHOP_TENANT_INSTANCES, e.g: WORKSHOP_TENANT_INSTANCES=(AAAA BBBB CCCC)"
+  exit 1
+fi 
+
 export KEPTN_ENDPOINT="${PREFIX}://${KEPTN_CONTROL_PLANE_DOMAIN}"
 export KEPTN_INGRESS="${KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN}"
 
 currentDir=pwd
 cd ../..
-./create-keptn-project-from-template.sh prod-delivery-simplenode ${OWNER_EMAIL} delivery-demo 3
+./create-keptn-project-from-template.sh prod-delivery-simplenode ${OWNER_EMAIL} delivery-demo "WORKSHOP_TENANT_INSTANCES[@]"
 cd 
