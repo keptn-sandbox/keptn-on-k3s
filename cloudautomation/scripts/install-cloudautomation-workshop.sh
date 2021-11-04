7ec464fe437a1ee76b056d0d951f4c4b6fc5e6f1
Andreas Grabner <grabnerandi@gmx.at>
2021-11-04 11:28:09 +0100

    updates
    Signed-off-by: Andreas Grabner <grabnerandi@gmx.at>

--- cloudautomation/scripts/install-cloudautomation-workshop.sh
+++ cloudautomation/scripts/install-cloudautomation-workshop.sh
@@ -5,11 +5,13 @@ set -eu
 PREFIX="https"
 
 KEPTN_CONTROL_PLANE_DOMAIN=${KEPTN_CONTROL_PLANE_DOMAIN:-none}
+KEPTN_CONTROL_PLANE_API_TOKEN=${KEPTN_CONTROL_PLANE_API_TOKEN:-none}
 KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN=${KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN:-none}
 
-if [[ "$KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN" == "none" ]] || [[ "$KEPTN_CONTROL_PLANE_DOMAIN" == "none" ]]; then
+if [[ "$KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN" == "none" ]] || [[ "$KEPTN_CONTROL_PLANE_DOMAIN" == "none" ]] || [[ "$KEPTN_CONTROL_PLANE_API_TOKEN" == "none" ]]; then
   echo "Script needs control plain domain set in KEPTN_CONTROL_PLANE_DOMAIN, e.g: abc12345.cloudautomation.live.dynatrace.com"
   echo "Script needs execution plane ingress domain set in KEPTN_EXECUTION_PLANE_INGRESS_DOMAIN, e.g: your.local.i.p.nip.io"
+  echo "Script needs keptn API token set in KEPTN_CONTROL_PLANE_API_TOKEN"
   exit 1
 fi
 
